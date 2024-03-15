4891dc931dfc590dd0e5f8807c0c9efb942557f7
Asheesh Kumar <asheesh220720@gmail.com>
2024-03-15 14:14:10 +0530

    Provider_Riverpod

--- lib/screens/first.dart
+++ lib/screens/first.dart
@@ -1,5 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sm_rp/riverpod/provider.dart';
 
+// Provider ref - communicates b/w one provider to another provider
+// WidgetRef ref - communicates from widget to another providers
+
+// StatelessWidget
+
+// 1st Method:
+/*
+class FirstScreen extends ConsumerWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(context, WidgetRef ref) {
+    final name = ref.watch(nameProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              name,
+              style: const TextStyle(
+                fontSize: 34,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
+
+/*
 class FirstScreen extends StatelessWidget {
   const FirstScreen({super.key});
 
@@ -10,11 +49,62 @@ class FirstScreen extends StatelessWidget {
         title: const Text('Provider'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Text('fhrh'),
-          ],
+        child: Consumer(
+          builder: (context, ref, child) {
+            final name = ref.watch(nameProvider);
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            );
+          },
         ),
+      ),
+    );
+  }
+}
+*/
+
+// StatefulWidget
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    //final name = ref.watch(nameProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider'),
+      ),
+      body: Center(
+        child: Consumer(builder: (context, ref, child) {
+          final name = ref.watch(nameProvider);
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                name,
+                style: const TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
